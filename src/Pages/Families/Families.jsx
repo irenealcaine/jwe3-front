331d277ea50b807e401a308f2d68e66ac7112abf
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2025-12-29 15:49:33 +0000

    families data

--- src/Pages/Families/Families.jsx
+++ src/Pages/Families/Families.jsx
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const FamliesPage = () => {
+  const [families, setFamilies] = useState({});
 
-    const [families, setFamilies] = useState([]);
+  useEffect(() => {
+    async function fetchFamilies() {
+      const res = await fetch(`https://jwe3-api.up.railway.app/api/families`);
+      const data = await res.json();
 
-    useEffect(() => {
-        async function fetchFamilies() {
-            const res = await fetch(`https://jwe3-api.up.railway.app/api/families`);
-            const data = await res.json();
+      //   const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
+      //   setFamilies(sortedData);
 
-            //   const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
-            //   setFamilies(sortedData);
+      console.log(data);
+      setFamilies(data);
+    }
+    fetchFamilies();
+  }, []);
 
-            console.log(data);
-            setFamilies(data);
+  return (
+    <div>
+      <h1>Families</h1>
+      {Object.entries(families).map(([familyName, familyData]) => (
+        <div key={familyName}>
+          <h2>{familyName}</h2>
 
-        }
-        fetchFamilies();
-    }, []);
-
-    return (
-        <div>
-            <h1>Families</h1>
-            {families.map((family) => (
-                <div key={family.name}>
-                    <h2>{family.name}</h2>
-                </div>
-            ))}
+          {familyData.dinosaurs.map((dinosaur) => (
+            <Link key={dinosaur.slug} to={`/dinosaurs/${dinosaur.slug}`}>
+              <h3>{dinosaur.name}</h3>
+              <img
+                src={`https://jwe3-api.up.railway.app${dinosaur.image}`}
+                alt={dinosaur.name}
+              />
+            </Link>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default FamliesPage;
